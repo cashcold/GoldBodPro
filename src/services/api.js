3755c1078c65b7123ab95c174941880319537e35
Frank99 <frankainoo@gmail.com>
2026-08-06 12:08:49 +0000

    Fix Axios API base URL for Heroku backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,33 @@
 import axios from 'axios';
 
-const API_BASE = '/api';
+const getBaseURL = () => {
+  const envUrl = import.meta.env.VITE_API_URL;
+  if (envUrl) {
+    return `${envUrl.replace(/\/$/, '')}/api`;
+  }
+  // Fallback to your Heroku app backend
+  return 'https://goldbodpro-5fb815b977d9.herokuapp.com/api';
+};
 
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: getBaseURL(),
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
 // Interceptor to attach JWT token
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('goldbod_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('goldbod_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
